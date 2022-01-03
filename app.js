9540d511b800842b0965b43d3aa8ee5c2e4961b0
Nitish Jha <nitishjha6519@gmail.com>
2022-01-03 14:52:21 +0530

    process.env.PORT added

--- app.js
+++ app.js
@@ -84,8 +84,15 @@ app.post("/login", function(req,res){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 
-app.listen(3000, function(){
-	console.log("server is running at port 3000");
+
+app.listen(port, function(){
+
+	console.log("server is running at port 8000");
+
 });
